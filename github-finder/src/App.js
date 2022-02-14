193630c16771ae623544cca26a15b4c026ecd8a5
Ryan Fickenscher <rsficken@gmail.com>
2022-02-14 15:25:55 -0700

    section 8

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,8 +1,27 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+
+import Home from "./pages/Home.js";
+import About from "./pages/About.js";
+import NotFound from "./pages/404.js";
+import Navbar from "./components/layout/Navbar.js";
+import Footer from "./components/layout/Footer.js";
+
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={ <Home /> } />
+            <Route path="/about" element={ <About /> } />
+            <Route path="/404" element={ <NotFound /> } />
+            <Route path="/*" element={ <NotFound /> } />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
